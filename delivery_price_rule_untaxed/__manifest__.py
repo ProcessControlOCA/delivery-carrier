eab4e9daa5b26e5be4ab7d79f21c405b14360f92
Francisco Trejo <88048271+soportetierranube@users.noreply.github.com>
2023-12-05 16:51:27 -0600

    [MIG]delivery_price_rule_untaxed

---
+++ delivery_price_rule_untaxed/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2018 Simone Rubino - Agile Business Group
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Delivery price rule untaxed",
+    "summary": "Este módulo permite al usuario utilizar el precio total sin impuestos de la orden de venta dentro de las reglas utilizadas por los métodos de entrega.",
+    "version": "16.0.1.0.2",
+    "category": "Stock",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["delivery"],
+}
